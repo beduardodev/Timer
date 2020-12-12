b597e0b0bd22720e63910be9c21af2ac2a884208
beduardo.dev <beduardo.dev@gmail.com>
2020-12-12 11:29:05 -0300

    Implementada função de parar.

--- app/js/timer.js
+++ app/js/timer.js
@@ -1,12 +1,14 @@
 const moment = require('moment');
 
 let segundos;
+let timer;
 
 module.exports = {
     iniciar(el) {
         let tempo = moment.duration(el.textContent);
         segundos = tempo.asSeconds();
-        setInterval(() => {
+        clearInterval(timer);
+        timer = setInterval(() => {
             segundos++;
             el.textContent = this.segundosParaTempo(segundos);
         }, 1000);
@@ -15,6 +17,6 @@ module.exports = {
         return moment().startOf('day').seconds(segundos).format("HH:mm:ss");
     },
     parar() {
-
+        clearInterval(timer);
     }
 }
